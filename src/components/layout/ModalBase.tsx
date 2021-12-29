7ff196328acf35e65a70be6d77bf960ddca6cdf3
shiropt <sh071332@gmail.com>
2021-12-29 16:54:49 +0900

    モーダルベースコンポーネント作成

---
+++ src/components/layout/ModalBase.tsx
@@ -0,0 +1,25 @@
+import type { ReactNode, VFC } from 'react'
+import { Modal, ModalOverlay, ModalContent, ModalHeader, ModalBody, ModalCloseButton } from '@chakra-ui/react'
+
+type PostModalProps = {
+  title: string
+  height: number
+  width: number
+  children: ReactNode
+  isOpen: boolean
+  closeButton: string
+  onClose: VoidFunction
+}
+
+export const ModalBase: VFC<PostModalProps> = (props) => {
+  return (
+    <Modal {...props} size="40px">
+      <ModalOverlay />
+      <ModalContent h={props.height} w={props.width}>
+        <ModalHeader color="#DD6B20">{props.title}</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody>{props.children}</ModalBody>
+      </ModalContent>
+    </Modal>
+  )
+}
