f8b16e12987db025bfbbafa989c284efb1670ed9
shiropt <sh071332@gmail.com>
2022-01-29 13:46:42 +0900

    ✅

--- src/axios.ts
+++ src/axios.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export default axios.create({
-  baseURL: `http://ec2-18-177-67-187.ap-northeast-1.compute.amazonaws.com:3030/`,
+  baseURL: `http://www.switter-prd.net:3030/`,
   // baseURL: `http://localhost:3030/`,
 })
